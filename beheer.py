71a5cdfabe351407bd6e85adf93dc12371bccf1e
josvanginkel <jos-vanginkel@hotmail.com>
2016-11-10 21:20:19 +0100

    Add files via upload

--- beheer.py
+++ beheer.py
@@ -1,5 +1,7 @@
 import os
-while True:
-    while True:
-        os.system("tweetbeheren.py 1")
-        break
+count = len(open('queue.csv').readlines())
+while count > 1:
+    count = len(open('queue.csv').readlines())
+    os.system("tweetbeheren.py 1")
+
+print('Op dit moment zijn er geen openstaande berichten.')
